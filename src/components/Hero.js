8bd2edbf4fe8ee6e99c34aa46eea567ef60917f8
smschijf <210578@student.glu.nl>
2022-11-07 17:03:40 +0100

    hero section almost done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,23 @@
+import Avatar from '../assets/img/avatar.png';
+
 const Hero = () => {
     return (
-        <section>
+        <section className="hero">
             <div className="heroText">
                 <h1>Hey! Ik ben Sander. 👋</h1>
                 <p>
-                    Een gedreven
-                    <span>developer</span>
-                    met een passie voor het maken van front-end applicaties. ✨
+                    Een gedreven <span>developer</span> met 
+                    een passie voor het maken van front-end applicaties. ✨
                 </p>
                 <p>
-                    Momenteel een student bij het
-                    <a href="https://www.glu.nl" target="_blank" rel="noreferrer">Grafisch Lyceum Utrecht</a>
+                    Momenteel student bij het <a href="https://www.glu.nl" target="_blank" rel="noreferrer"> Grafisch Lyceum Utrecht</a>. 👨🏻‍💻
+                </p>
+                <p>
+                    <span className="smallerText">Psst! mijn portfolio is een WIP, dus bekijken op desktop is het beste voor nu!</span>
                 </p>
             </div>
             <div className="heroImage">
-                <img src="./" alt="profile"></img>
+                <img src={Avatar} alt="avatar"></img>
             </div>
         </section>
     );
