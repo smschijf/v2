84a3ea9aa03131b8ebe24df6da78d179f2f1ecd8
smschijf <210578@student.glu.nl>
2022-11-11 16:04:20 +0100

    contact section done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import AboutMe from './components/AboutMe';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
             <Projects />
             <AboutMe />
         </main>
+        <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,78 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+const Contact = () => {
+  return (
+    <section className="contact">
+      <div className="contactContainer">
+        <div className="contactInformation">
+          <h2>Neem Contact Op! 📝</h2>
+          <p>
+            Je kan mij vinden op social media, of je kan mij hier een bericht
+            sturen!
+          </p>
+          <ul className="contactIcons">
+            <li>
+              <a href="https://www.linkedin.com/in/sander-schijf-90b70a242/">
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/smschijf">
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/smschijf">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="contactForm">
+          <form action="#" method="post" name="contact">
+            <label htmlFor="name" className="hidden">
+              naam
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="naam"
+              autoComplete="off"
+              required
+            />
+            <label htmlFor="email" className="hidden">
+              email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="email"
+              required
+            />
+            <label htmlFor="message" className="hidden">
+              bericht
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="bericht"
+              required
+            ></textarea>
+            <div className="submitContainer">
+              <button type="submit" className="button">
+                verzend bericht
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
